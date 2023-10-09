bf803babe6aa034c1a9ab82ea2e2151a57f26c62
papihoudini <ochiengcliff.co@gmail.com>
2023-10-09 11:27:27 -0400

    Asynchronous function

---
+++ 0x01-python_async_function/async-prac.py
@@ -0,0 +1,23 @@
+import asyncio
+
+# Synchronous programming
+# def foo():
+#     return
+
+
+# foo()
+# print("Papi")
+
+async def main():
+    print("Papi")
+    # await foo("text")
+    task = asyncio.create_task(foo("text"))
+    # await task
+    await asyncio.sleep(2)
+    print("Finished")
+
+async def foo(text):
+    print(text)
+    await asyncio.sleep(1)
+
+asyncio.run(main())
