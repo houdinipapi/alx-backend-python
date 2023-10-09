40247cd76e8255b8b58329515d27a49768a76a54
papihoudini <ochiengcliff.co@gmail.com>
2023-10-09 13:16:48 -0400

    How to run concurrent coroutines

---
+++ 0x01-python_async_function/async-create.py
@@ -0,0 +1,20 @@
+import asyncio
+
+async def coroutine1():
+    await asyncio.sleep(2)
+    print("Coroutine 1 finished")
+
+
+async def coroutine2():
+    await asyncio.sleep(1)
+    print("Coroutine 2 finished")
+
+async def main():
+    task1 = asyncio.create_task(coroutine1())
+    task2 = asyncio.create_task(coroutine2())
+
+    await task1
+    await task2
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ 0x01-python_async_function/async-gather.py
@@ -0,0 +1,18 @@
+import asyncio
+
+async def coroutine1():
+    await asyncio.sleep(2)
+    print("Coroutine 1 finished")
+
+async def coroutine2():
+    await asyncio.sleep(1)
+    print("Coroutine 2 finished")
+
+async def main():
+    await asyncio.gather(
+        coroutine1(),
+        coroutine2()
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
